a53afe407608cb9382e4d827243beeb95218c618
vanshdubey <vanshdubey101963@gmail.com>
2026-01-16 21:31:04 +0530

    renamed common/src/* to common/ and fixed the aws access key error

--- packages/common/aws-s3.ts
+++ packages/common/aws-s3.ts
@@ -1,5 +1,22 @@
-import { S3 } from "@aws-sdk/client-s3";
+import { S3, S3Client } from "@aws-sdk/client-s3";
+
+// const s3Client = new S3Client({
+//   region: "us-east-1",
+//   endpoint: process.env.STORAGE_ENDPOINT,
+//   credentials: {
+//     accessKeyId: process.env.STORAGE_ACCESS_KEY,
+//     secretAccessKey: process.env.STORAGE_SECRET_KEY
+//   },
+//   forcePathStyle: true
+// });
 const s3Client = new S3({
-    endpoint: process.env.STORAGE_ENDPOINT,
+  region: "us-east-1",
+  endpoint: process.env.STORAGE_ENDPOINT,
+    credentials: {
+      accessKeyId: process.env.STORAGE_ACCESS_KEY,
+      secretAccessKey: process.env.STORAGE_SECRET_KEY
+    },
+  forcePathStyle: true
 });
+
 export default s3Client;
