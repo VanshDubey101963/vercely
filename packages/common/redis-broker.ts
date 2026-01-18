ebfacc2abce64718c20e35f0bc242a66839fc1de
vanshdubey <vanshdubey101963@gmail.com>
2026-01-18 21:56:01 +0530

    Added code for redis queue operations and connections

---
+++ packages/common/redis-broker.ts
@@ -0,0 +1,6 @@
+import { createClient, RedisClientType } from "redis";
+const redisClient: RedisClientType = createClient()
+redisClient.on('error', err => console.log('Redis Client Error', err));
+await redisClient.connect()
+
+export default redisClient
